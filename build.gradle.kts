e1e403d85cd857cfa53e843cd48b6e072de4bf72
Daniele Conti <fourlastor@users.noreply.github.com>
2023-03-29 14:52:15 +0100

    Update gradle config

---
+++ build.gradle.kts
@@ -0,0 +1,35 @@
+@Suppress(
+    // known false positive: https://youtrack.jetbrains.com/issue/KTIJ-19369
+    "DSL_SCOPE_VIOLATION"
+)
+plugins {
+    alias(libs.plugins.nexus.publish)
+}
+
+buildscript {
+    repositories {
+        mavenLocal()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
+val libVersion: String by project
+
+allprojects {
+    repositories {
+        mavenLocal()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+    version = libVersion
+}
+
+group = "io.github.fourlastor.gdx"
+version = libVersion
+
+nexusPublishing {
+    repositories {
+        sonatype()
+    }
+}
