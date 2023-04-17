d6379e816b69c0c307da52f87d80cdf4b94b6699
Daniele Conti <fourlastor@users.noreply.github.com>
2023-04-17 08:02:46 +0100

    Make ActorComponent Poolable

--- harlequin-ashley/src/main/java/io/github/fourlastor/harlequin/component/ActorComponent.java
+++ harlequin-ashley/src/main/java/io/github/fourlastor/harlequin/component/ActorComponent.java
@@ -2,14 +2,27 @@ package io.github.fourlastor.harlequin.component;
 
 import com.badlogic.ashley.core.Component;
 import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.utils.Pool;
 
-public class ActorComponent implements Component {
+public class ActorComponent implements Component, Pool.Poolable {
 
-    public final Actor actor;
-    public final Enum<?> layer;
+    public Actor actor;
+    public Enum<?> layer;
+
+    public ActorComponent() {}
 
     public ActorComponent(Actor actor, Enum<?> layer) {
+        initialize(actor, layer);
+    }
+
+    public void initialize(Actor actor, Enum<?> layer) {
         this.actor = actor;
         this.layer = layer;
+    }
+
+    @Override
+    public void reset() {
+        this.actor = null;
+        this.layer = null;
     }
 }
